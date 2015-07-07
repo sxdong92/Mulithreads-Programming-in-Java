59dcbb435403035577a1d7c7931d5645acdf4902
sxdong92 <sxdong92@gmail.com>
2015-07-07 00:41:36 -0400

    modify

--- src/threadSignaling/MyWaitNotify2.java
+++ src/threadSignaling/MyWaitNotify2.java
@@ -1,5 +1,9 @@
 package threadSignaling;
 
+/**
+ * 如果一个线程先于被通知线程调用wait()前调用了notify()，等待的线程将错过这个信号。在某些情况下，这可能使等待线程永远在等待，不再醒来.
+ * 为了避免丢失信号，必须把它们保存在信号类里。在MyWaitNotify的例子中，通知信号应被存储在MyWaitNotify实例的一个成员变量里。
+ */
 public class MyWaitNotify2 {
 	MonitorObject myMonitorObject = new MonitorObject();
 	boolean wasSignalled = false;
