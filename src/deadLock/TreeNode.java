691eb170bae88bee2c57060340ad9b61e220d295
sxdong92 <sxdong92@gmail.com>
2015-07-07 01:40:11 -0400

    dead lock

---
+++ src/deadLock/TreeNode.java
@@ -0,0 +1,34 @@
+package deadLock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 如果线程1调用parent.addChild(child)方法的同时有另外一个线程2调用child.setParent(parent)方法，两个线程中的parent(或child)表示的是同一个对象，此时就会发生死锁。
+ */
+public class TreeNode {
+	TreeNode parent   = null;
+	List<TreeNode> children = new ArrayList<TreeNode>();
+	
+	public synchronized void addChild(TreeNode child) {
+		if(!this.children.contains(child)) {
+			this.children.add(child);
+			child.setParentOnly(this);
+		}
+	}
+	
+	public synchronized void addChildOnly(TreeNode child) {
+		if(!this.children.contains(child)) {
+			this.children.add(child);
+		}
+	}
+	
+	public synchronized void setParent(TreeNode parent) {
+		this.parent = parent;
+		parent.addChildOnly(this);
+	}
+	
+	public synchronized void setParentOnly(TreeNode parent) {
+		this.parent = parent;
+	}
+}
--- src/threadSignaling/MyWaitNotify3.java
+++ src/threadSignaling/MyWaitNotify3.java
@@ -8,6 +8,11 @@ package threadSignaling;
  * 被唤醒的线程会自旋直到自旋锁(while循环)里的条件变为false。
  * 
  * 留意wait()方法是在while循环里，而不在if表达式里。如果等待线程没有收到信号就唤醒，wasSignalled变量将变为false,while循环会再执行一次，促使醒来的线程回到等待状态。
+ * 
+ * 如果你有多个线程在等待，被notifyAll()唤醒，但只有一个被允许继续执行，使用while循环也是个好方法。
+ * 每次只有一个线程可以获得监视器对象锁，意味着只有一个线程可以退出wait()调用并清除wasSignalled标志（设为false）。
+ * 一旦这个线程退出doWait()的同步块，其他线程退出wait()调用，并在while循环里检查wasSignalled变量值。
+ * 但是，这个标志已经被第一个唤醒的线程清除了，所以其余醒来的线程将回到等待状态，直到下次信号到来。
  */
 public class MyWaitNotify3 {
 	MonitorObject myMonitorObject = new MonitorObject();
