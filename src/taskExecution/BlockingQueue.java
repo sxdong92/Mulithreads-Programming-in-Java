dc9dd60db8c83faeab7b49b01974267e63fff488
sxdong92 <sxdong92@gmail.com>
2015-07-13 01:47:01 -0400

    modify

--- src/taskExecution/BlockingQueue.java
+++ src/taskExecution/BlockingQueue.java
@@ -1,4 +1,4 @@
-package synchronizer;
+package taskExecution;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -6,16 +6,17 @@ import java.util.Queue;
 /**
  * 必须注意到，在enqueue和dequeue方法内部，只有队列的大小等于上限（limit）或者下限（0）时，才调用notifyAll方法。
  * 如果队列的大小既不等于上限，也不等于下限，任何线程调用enqueue或者dequeue方法时，都不会阻塞，都能够正常的往队列中添加或者移除元素。 
+ * @param <E>
  */
-public class BlockingQueue {
-	private Queue queue = new LinkedList();
+public class BlockingQueue<E> {
+	private Queue<E> queue = new LinkedList<E>();
 	private int  limit = 10;
 	
 	public BlockingQueue(int limit) {
 		this.limit = limit;
 	}
 	
-	public synchronized void enqueue(Object item) throws InterruptedException {
+	public synchronized void enqueue(E item) throws InterruptedException {
 		while(this.queue.size() == this.limit) {
 			wait();
 		}
@@ -25,7 +26,7 @@ public class BlockingQueue {
 		this.queue.add(item);
 	}
 	
-	public synchronized Object dequeue() throws InterruptedException {
+	public synchronized E dequeue() throws InterruptedException {
 		while(this.queue.size() == 0) {
 			wait();
 		}
--- src/taskExecution/ThreadPool.java
+++ src/taskExecution/ThreadPool.java
@@ -1,5 +1,62 @@
 package taskExecution;
 
-public class ThreadPool {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ThreadPool {
+	private BlockingQueue taskQueue = null;
+	private List<PoolThread> threads = new ArrayList<PoolThread>();
+	private boolean isStopped = false;
+	
+	public ThreadPool(int noOfThreads, int maxNumOfTasks) {
+		taskQueue = new BlockingQueue(maxNumOfTasks);
+		for (int i=0; i<noOfThreads; i++) {
+			threads.add(new PoolThread(taskQueue));
+		}
+		for (PoolThread thread : threads) {
+			thread.start();
+		}
+	}
+	
+	public synchronized void execute(Runnable task) throws InterruptedException {
+		if(this.isStopped) throw new IllegalStateException("ThreadPool is stopped");
+		this.taskQueue.enqueue(task);
+	}
+	
+	public synchronized void stop() {
+		this.isStopped = true;
+		for (PoolThread thread : threads) {
+			thread.stop();
+		}
+	}
+}
+
+class PoolThread extends Thread {
+	private BlockingQueue<Runnable> taskQueue = null;
+	private boolean       isStopped = false;
+	
+	public PoolThread(BlockingQueue<Runnable> queue) {
+		taskQueue = queue;
+	}
+	
+	public void run() {
+		while (!isStopped()) {
+			try {
+				Runnable runnable = taskQueue.dequeue();
+				runnable.run();
+			} catch(Exception e) {
+				// 写日志或者报告异常,
+				// 但保持线程池运行.
+			}
+		}
+	}
+	
+	public synchronized void toStop() {
+		isStopped = true;
+		this.interrupt(); // 打断池中线程的 dequeue() 调用.
+	}
+	
+	public synchronized boolean isStopped() {
+		return isStopped;
+	}
 }
